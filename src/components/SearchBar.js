ca78595caf9a6fb0ca54022187c54252fcb25065
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-01-16 18:15:12 -0800

    finished

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,26 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
+import "./SearchBar.css";
+import { useState } from "react";
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(term);
+  };
+
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+  };
+
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleFormSubmit} action="">
+        <label htmlFor="">Enter Search Term</label>
+        <input value={term} type="text" onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
